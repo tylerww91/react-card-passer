1b2e0ff727ebcd7af46f3fc2084f2e0771ef1b8e
Tyler Watson <tylerww91@gmail.com>
2023-01-25 12:36:21 -0800

    builds GameContext.js which exports the shared selectedCard state and setSelectedCard setter through context

---
+++ src/context/GameContext.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import { createContext } from 'react';
+
+const GameContext = createContext();
+
+const GameProvider = ({ children }) => {
+  const [selectedCard, setSelectedCard] = useState();
+
+  return (
+    <GameContext.Provider value={{ selectedCard, setSelectedCard }}>
+      {children}
+    </GameContext.Provider>
+  );
+};
+
+export { GameProvider, GameContext };
